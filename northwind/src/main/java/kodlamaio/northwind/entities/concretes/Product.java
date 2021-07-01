95d521433749cedc0e25cd74d745eae582b5bb13
evinoguz <oguzevin@gmail.com>
2021-07-01 17:17:36 +0300

    gun6

---
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -0,0 +1,38 @@
+package kodlamaio.northwind.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data //getter & setter
+@Entity //veritabani nesnesi
+@Table(name="products")
+public class Product {
+
+	@Id //tablonun idsi
+	@GeneratedValue //otomatik 1'er artir
+	
+	@Column(name="product_id")
+	private int id;
+	
+	@Column(name="category_id")
+	private int categoryId;
+	
+	@Column(name="product_name")
+	private String productName;
+	
+	@Column(name="unit_price")
+	private double unitPrice;
+	
+	@Column(name="units_in_stock")
+	private short unitsInStock;
+	
+	@Column(name="quantity_per_unit")
+	private String quantityPerUnit;
+	
+}
+ 
